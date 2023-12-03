61941e79e0042d6df4cfe62c9627fd1ed3fea2ec
Yibing (Amy) Lu <yibingamy@gmail.com>
2023-12-03 16:10:52 -0500

    match pet details with form placeholders

--- frontend/src/routes/PetDetails.jsx
+++ frontend/src/routes/PetDetails.jsx
@@ -2,50 +2,48 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "../components/NavBar";
 import classes from "./PetDetails.module.css";
-import Endpoints from "../constants/Endpoints"
-import { useParams } from 'react-router-dom';
+import Endpoints from "../constants/Endpoints";
+import { useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 function PetDetails() {
-    const [petDetails, setPetDetails] = useState({
-        name: "",
-        gender: "",
-        birthday: "",
-        status: "",
-        medical_history: "",
-        special_needs: "",
-        personality: "",
-    });
+  const [petDetails, setPetDetails] = useState({
+    name: "",
+    gender: "",
+    birthday: "",
+    status: "",
+    medical_history: "",
+    special_needs: "",
+    personality: "",
+  });
 
-    const { pk } = useParams();
-    const endpoint = Endpoints.pet.replace(":pk", pk);
+  const { pk } = useParams();
+  const endpoint = Endpoints.pet.replace(":pk", pk);
 
-    useEffect(() => {
-        // Fetch pet details from the server
-       
-        const fetchPetDetails = async () => {
-            try {
+  useEffect(() => {
+    // Fetch pet details from the server
 
-                const response = await axios.get(endpoint); 
-                console.log("Pet details", response.data);
+    const fetchPetDetails = async () => {
+      try {
+        const response = await axios.get(endpoint);
+        console.log("Pet details", response.data);
 
-                // Assuming API response has a structure like { name, gender, birthday, status, medicalHistory, specialNeeds, personality }
-                setPetDetails(response.data);
-            } catch (error) {
-                console.error("Error fetching pet details:", error);
-            }
-        };
+        // Assuming API response has a structure like { name, gender, birthday, status, medicalHistory, specialNeeds, personality }
+        setPetDetails(response.data);
+      } catch (error) {
+        console.error("Error fetching pet details:", error);
+      }
+    };
 
-        fetchPetDetails();
-    }); // Empty dependency array ensures this effect runs once when the component mounts
-
+    fetchPetDetails();
+  }); // Empty dependency array ensures this effect runs once when the component mounts
 
   return (
     <body className={classes["page-container"]}>
       <NavBar />
       <content className={classes["pet-details-content"]}>
-              {/* <h1>{petDetails.name} ({petDetails.gender})</h1> */}
-              <h1>{petDetails.name} </h1>
+        {/* <h1>{petDetails.name} ({petDetails.gender})</h1> */}
+        <h1>{petDetails.name} </h1>
 
         <div className={classes["derpycat"]}>
           <img src={require("../images/derpycat.png")} alt="cat" />
@@ -53,9 +51,35 @@ function PetDetails() {
 
         <div className={classes["grid-container"]}>
           <p>
-            <b>Status: </b>
+            <b>Status of Adoption: </b>
             {petDetails.status}
           </p>
+
+          <p>
+            <b>Species: </b>
+            {petDetails.species}
+          </p>
+
+          <p>
+            <b>Breed: </b>
+            {petDetails.breed}
+          </p>
+
+          <p>
+            <b>Age: </b>
+            {petDetails.age}
+          </p>
+
+          <p>
+            <b>Size (in kg): </b>
+            {petDetails.size}
+          </p>
+
+          <p>
+            <b>Gender: </b>
+            {petDetails.gender}
+          </p>
+
           <p>
             <b>Birthday: </b>
             {petDetails.birthday}
@@ -66,14 +90,21 @@ function PetDetails() {
             {petDetails.medical_history}
           </p>
           <p>
-            <b>Special Needs/Requirements: </b>
+            <b>Special needs or requirements: </b>
             {petDetails.special_needs}
           </p>
           <p>
-            <b>Personality: </b>
+            <b>Behaviour / Personality: </b>
             {petDetails.personality}
           </p>
-          <NavLink className={classes["adopt-button"]} to={`/petapplication`}>ADOPT NOW</NavLink>
+
+          <p>
+            <b>Other description: </b>
+            {petDetails.others}
+          </p>
+          <NavLink className={classes["adopt-button"]} to={`/petapplication`}>
+            ADOPT NOW
+          </NavLink>
           {/* <a href="pet-applications.html">
             <button className={classes["adopt-button"]}>ADOPT NOW</button>
           </a> */}
