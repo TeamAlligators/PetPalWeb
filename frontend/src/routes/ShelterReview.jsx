5a66b929d6babd57cf01871b6d06b585094563c5
Lucy <lucia.kim0816@gmail.com>
2023-12-05 01:14:01 -0500

    shelter details except pagination

--- frontend/src/routes/ShelterReview.jsx
+++ frontend/src/routes/ShelterReview.jsx
@@ -12,6 +12,7 @@ function ShelterReview() {
     const [shelterData, setShelterData] = useState(null);
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState("");
+    const [newRating, setNewRating] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,79 +23,36 @@ function ShelterReview() {
                 setShelterData(shelterResponse.data);
                 setComments(commentsResponse.data.results);
 
-                const userIds = commentsResponse.data.results.map(comment => comment.user);
-
-                const users = [];
-
-                for (const userId of userIds) {
-                    // Check if the user is a seeker
-                    const seekerResponse = await axios.get(Endpoints.seekers);
-                    const seekerUser = seekerResponse.data.find((currUser) => currUser.id === userId);
-                    if (seekerUser) {
-                        users.push(seekerUser);
-                        continue; // Move to the next iteration
-                    }
-
-                    // Check if the user is a shelter
-                    const shelterResponse = await axios.get(Endpoints.shelters);
-                    const shelterUser = shelterResponse.data.find((currUser) => currUser.id === userId);
-                    if (shelterUser) {
-                        users.push(shelterUser);
-                    }
-                }
-
-                setComments((prevComments) =>
-                    prevComments.map((comment) => ({
-                        ...comment,
-                        user: users.find((user) => user.id === comment.user),
-                    }))
-                );
+                console.log("shelterResponse", shelterResponse);
+                console.log("commentsResponse", commentsResponse);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
-
         fetchData();
     }, [pk]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
-            // Make a POST request to create a new comment
             const response = await axios.post(Endpoints.sheltercomments.replace(":pk", pk), {
                 content: newComment,
+                rating: newRating
             }, {
                 headers: {
                     Authorization: `Bearer ${user.token}`,
                 },
             });
-
-            const userDetailsResponse = {
-                data: {
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    account_type: user.account_type,
-                },
-            }
-            const userDetails = userDetailsResponse.data;
-
-            // Add the new comment to the existing comments with user details
-            setComments((prevComments) => [
-                ...prevComments,
-                { id: response.data.id, user: userDetails, content: newComment },
-            ]);
-
-            // Clear the input field
+            console.log("Comment response:", response);
+            // fetch the comment data again
+            const commentsResponse = await axios.get(Endpoints.sheltercomments.replace(":pk", pk));
+            setComments(commentsResponse.data.results);
             setNewComment("");
+            setNewRating(null);
         } catch (error) {
             console.error('Error submitting comment:', error);
         }
     };
-
-    const handleReplyClick = (commentId) => {
-        console.log("Replying to comment:", commentId);
-    }
 
     return (
         <body className={styles.pageContainer}>
@@ -124,7 +82,23 @@ function ShelterReview() {
                         <h2 className={styles.reviews}>Reviews</h2>
                         <form className={styles.newReview} onSubmit={handleSubmit}>
                             <p id="review-name">{user.userId ? `${user.first_name} ${user.last_name}` : 'Please log in'}</p>
-
+                            <div className={styles.gridItem}>
+                                <label className={styles.ratingLabel} htmlFor="review-rating">Rating:</label>
+                                <select
+                                    className={styles.rating}
+                                    id="review-rating"
+                                    name="rating"
+                                    value={newRating}
+                                    onChange={(e) => setNewRating(e.target.value === "noRating" ? null : parseInt(e.target.value))}
+                                >
+                                    <option value="noRating">No Rating</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                            </div>
                             <div className={styles.gridItem}>
                                 <input
                                     id="review-content"
@@ -144,13 +118,9 @@ function ShelterReview() {
 
                         {comments && comments.map(comment => (
 
-                            <div key={comment.id} className={`${user.userId === pk ? styles.shelterOwnerComment : styles.commentItem}`}>
-                                <p>{comment.user && `${comment.user.first_name} ${comment.user.last_name} (${comment.user.account_type})`}</p>
+                            <div key={comment.id} className={`${user.userId === shelterData.shelter.id ? styles.shelterOwnerComment : styles.commentItem}`}>
+                                <p>{comment.user && `${comment.user_name} (${comment.user_type})`} - {comment.rating ? `Rating: ${comment.rating}/5` : 'No Rating'}</p>
                                 <p>{comment.content}</p>
-                                <div className={styles.gridItem2}>
-                                    <button className={styles.save} onClick={() => handleReplyClick(comment.id)} disabled={!user.userId}>Reply</button>
-                                    {/* Add a reply form or handle reply logic */}
-                                </div>
                             </div>
                         ))}
                     </div>
