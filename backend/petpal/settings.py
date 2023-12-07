f808c97144633c515d1b066cdd3e28ef4a6b758e
Ljime <Ljime@users.noreply.github.com>
2023-12-07 04:26:40 -0500

    com

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -81,8 +81,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'petpal.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
