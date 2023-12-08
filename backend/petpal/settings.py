8471aaecd6d6b6c9327736e3553a576c2e9de133
Lucy <lucia.kim0816@gmail.com>
2023-12-08 16:32:15 -0500

    LAST LAST TRY

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -151,13 +151,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATIC_URL = '../frontend/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, '../frontend'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', '../frontend')
+# STATIC_URL = '../frontend/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, '../frontend'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', '../frontend')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
