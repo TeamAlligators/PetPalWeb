cd70431234165dc759e8b8185988ac3251252701
Lucy <lucia.kim0816@gmail.com>
2023-12-08 16:25:14 -0500

    is this gonna work

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,9 @@
+# Create and activate virtual environment
+virtualenv venv
+# On Windows
+source venv/Scripts/activate
+# # On macOS/Linux
+# source venv/bin/activate
+
 pip install -r packages.txt
-python3.10 manage.py collectstatic
+python manage.py collectstatic
