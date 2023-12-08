48ee94178ff3ff729956237be9879f8aab482f35
Lucy <lucia.kim0816@gmail.com>
2023-12-08 18:03:53 -0500

    help me.

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -54,11 +54,13 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.56.1:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://192.168.56.1:3000"
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True  
 
 ROOT_URLCONF = 'petpal.urls'
 
