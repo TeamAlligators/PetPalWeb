9b85d9a81214e83ba88ac0de4cb8496f84b6d589
Lucy <lucia.kim0816@gmail.com>
2023-12-11 05:02:11 -0500

    deployed ver

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'petpal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         "USER": 'postgres',
-#         "PASSWORD": 'eFdcDc6eCGA3aCC46CAfcgEf*CB-DGgb',
-#         'HOST': 'roundhouse.proxy.rlwy.net',
-#         'PORT': '41581'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        "USER": 'postgres',
+        "PASSWORD": 'eFdcDc6eCGA3aCC46CAfcgEf*CB-DGgb',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '41581'
+    }
+}
 
 
 AUTH_USER_MODEL = "app.CustomUser"
